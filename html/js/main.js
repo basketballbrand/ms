0513c63fcb17897efecc75d77103e6aedb68da48
Alan <tsaichunglun@gmail.com>
2021-02-01 00:08:19 +0800

    ms_0201
    ms_0201

--- html/js/main.js
+++ html/js/main.js
@@ -1,9 +1,13 @@
 $('.nav .menu').on('click',function(){
-    $(this).addClass('active');
+    $(this).next('.function_bar').animate({
+        left:"0"
+    },500)
     $('.nav .overlay').removeClass('unblock');
 })
 $('.fa-times , .nav .overlay').on('click',function(){
-    $('.nav .menu').removeClass('active');
+    $('.nav .menu').next('.function_bar').animate({
+        left:"-75%"
+    },200)
     $('.nav .overlay').addClass('unblock');
 })
 $('a.scroll-link').click(function(e){
